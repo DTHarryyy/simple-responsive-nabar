0072c1cde2d6b8bac91c9b8874252fd79899838e
Harry De Torres <165980069+DTHarryyy@users.noreply.github.com>
2024-08-17 19:55:19 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function showSideBar(){
+    document.querySelector(".sidebar").style.display = "block";
+}
+function closeSideBar(){
+    let nav = document.querySelector(".sidebar")
+    nav.style.display = "none";
+}
